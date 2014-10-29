b4bf8941e030994cc16ee3f1a27cd822c4aa5b54
Rachel Knowler <rachel.knowler@liip.ch>
2014-10-29 15:39:58 +0100

    Added new datasets from the GIS.

--- ckanext/zhgis/harvesters/zhgisharvester.py
+++ ckanext/zhgis/harvesters/zhgisharvester.py
@@ -23,197 +23,172 @@ class ZhGisHarvester(HarvesterBase):
 
     DATASETS = {
         'c80f283d-6ab8-4ce4-a480-c7995c575b24': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=994',  # noqa
-            'tags': [u'Archäologie', u'archäologische Zonen']
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=994',
+            'tags': ['Archäologie', 'archäologische Zonen']
         },
         '1eac72b1-068d-4272-b011-d0010cc4bf676': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=691',  # noqa
-            'tags': [
-                'Denkmalschutz',
-                'Denkmalpflege',
-                'Inventar',
-                'Schutzliste',
-                'Objektliste'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=691',
+            'tags': ['Denkmalschutz', 'Denkmalpflege', 'Inventar', 'Schutzliste', 'Objektliste']
         },
         '0ef6823f-f46e-4df2-966b-3c52af8f1310': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=935',  # noqa
-            'tags': [
-                'Denkmalschutz',
-                'Denkmalpflege',
-                'Inventar',
-                'Schutzliste',
-                'Objektliste'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=935',
+            'tags': ['Denkmalschutz', 'Denkmalpflege', 'Inventar', 'Schutzliste', 'Objektliste']
         },
         '3a4782f1-d6c3-402b-bea0-8a160c3347491': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=265',  # noqa
-            'tags': ['Ortsbild', 'Ortsbildschutz', u'schutzwürdig']
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=265',
+            'tags': ['Ortsbild', 'Ortsbildschutz', 'schutzwürdig']
         },
         'f454fd4d-d47d-47b6-b09b-21c7c865e61b3': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=290',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=290',
             'tags': ['Waldvegetation', 'Wald']
         },
         '690425da-b1bc-4f30-be01-c0a4af4f8d5f': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1057',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1057',
             'tags': ['Schadstoffbelastung', 'Emissionen', 'Immissionen']
         },
         '238d1b4f-0039-4dc2-a171-c7b1bc72a2be': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1058',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1058',
             'tags': ['Schadstoffbelastung', 'Emissionen', 'Immissionen']
         },
         '8eff8b9a-960a-42cb-afd0-daeb5f70813a': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1054',  # noqa
-            'tags': [
-                'Gemeinden',
-                'Gemeindegrenzen',
-                'administrative Grenzen',
-                'Hoheitsgrenzen'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1054',
+            'tags': ['Gemeinden', 'Gemeindegrenzen', 'administrative Grenzen', 'Hoheitsgrenzen']
         },
         'b3bd50ae-b026-40a0-8b39-1cbcd4c4ac98': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1055',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1055',
             'tags': ['Haltestelle', 'ZVV', 'VBZ']
         },
         '8802b5b2-5c6a-46ad-9bf2-66757ebf04af': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1056',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1056',
             'tags': ['Haltestelle', 'ZVV', 'VBZ']
         },
         '81d38577-6635-4394-803b-334d616967aa': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1061',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1061',
             'tags': ['Siedlung', 'Siedlungsverzeichnis', 'Statistik']
         },
         '33cfa26c-c568-4972-bbc7-14ae52f9b319': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=956',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=956',
             'tags': []
         },
         '68a3f148-b3f5-4ba3-87d5-a26aafc03c14': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=955',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=955',
             'tags': []
         },
         'c34149b8-1ffe-46de-a3eb-01acb3fbeac5': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=870',  # noqa
-            'tags': [
-                u'Vögel',
-                'Feuchtgebiet',
-                'Ried',
-                'Flachmoor',
-                'Feuchtwiese',
-                'Heuschrecken',
-                'Geomorphologie',
-                'Geotope',
-                'Vegetationskartierung',
-                'Feuchtgebiete',
-                'Trockenstandorte',
-                'Libellen',
-                'Naturschutz',
-                'Biotope',
-                'Lichter Wald',
-                'Aktionsplan',
-                'Zielarten',
-                'Reptilien',
-                'Landschaftsschutz',
-                'Schutzverordnung',
-                'Pufferzone',
-                'Tagfalter',
-                'Sommervogel'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=870',
+            'tags': ['Vögel', 'Feuchtgebiet', 'Ried', 'Flachmoor', 'Feuchtwiese', 'Heuschrecken', 'Geomorphologie', 'Geotope', 'Vegetationskartierung', 'Feuchtgebiete', 'Trockenstandorte', 'Libellen', 'Naturschutz', 'Biotope', 'Lichter Wald', 'Aktionsplan', 'Zielarten', 'Reptilien', 'Landschaftsschutz', 'Schutzverordnung', 'Pufferzone', 'Tagfalter', 'Sommervogel']
         },
         'b83d1172-bed6-4e04-a56c-fcc55995dbc9': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=931',  # noqa
-            'tags': [
-                'Naturschutz',
-                'Biotope',
-                'Lichter Wald',
-                'Aktionsplan',
-                'Zielarten'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=931',
+            'tags': ['Naturschutz', 'Biotope', 'Lichter Wald', 'Aktionsplan', 'Zielarten']
         },
         'fb796034-407d-4f8d-906b-3a1b52f828da': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=932',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=932',
             'tags': []
         },
         '22550ba0-da1e-433e-a39e-ee95607b19d6': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=930',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=930',
             'tags': []
         },
         'dff63f01-001a-4416-ae0f-9cb6ce14ffe6': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=913',  # noqa
-            'tags': [
-                'Naturschutz',
-                'Landschaftsschutz',
-                'Schutzverordnung',
-                'Pufferzone'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=913',
+            'tags': ['Naturschutz', 'Landschaftsschutz', 'Schutzverordnung', 'Pufferzone']
         },
         '668e0bda-f150-48b0-aa05-38a5eac3cc16': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=933',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=933',
             'tags': []
-        },
-        '1ba987a4-0236-44e2-b5c0-f6cc5f167981': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1120',  # noqa
-            'tags': [
-                'Strasse',
-                'Strassenverkehr',
-                'Verkehrsnetz',
-                'Strassennetz',
-                'Nationalstrassen',
-                'Staatsstrassen',
-                'Kanton Zürich'
-            ]
         },
         '79bffc8f-e721-47d1-9ebf-d8a47c4cf3d0': {
-                'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1077',  # noqa
-                'tags': [
-                    'Neophyten',
-                    'invasiv',
-                    'Asiatischer Staudenknöterich',
-                    'Riesenbärenklau',
-                    'Essigbaum',
-                    'Ambrosia',
-                    'Goldrute',
-                    'Drüsiges Springkraut'
-                ]
+        'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1077',
+        'tags': ['Neophyten', 'invasiv', 'Asiatischer Staudenknöterich', 'Riesenbärenklau', 'Essigbaum', 'Ambrosia', 'Goldrute', 'Drüsiges Springkraut']
+        },
+        '1ba987a4-0236-44e2-b5c0-f6cc5f167981': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1120',
+            'tags': ['Strasse', 'Strassenverkehr', 'Verkehrsnetz', 'Strassennetz', 'Nationalstrassen', 'Staatsstrassen', 'Kanton Zürich']
         },
         '893345ff-8c84-4495-9b2f-53c0ec8390f9': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1119',  # noqa
-            'tags': [
-                'Strasse',
-                'Strassenverkehr',
-                'Verkehrsnetz',
-                'Strassennetz',
-                'Nationalstrassen',
-                'Staatsstrassen',
-                'Kanton Zürich'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1119',
+            'tags': ['Strasse', 'Strassenverkehr', 'Verkehrsnetz', 'Strassennetz', 'Nationalstrassen', 'Staatsstrassen', 'Kanton Zürich']
         },
         '3233bf0f-c87d-404f-ac34-98df45d5dc90': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1118',  # noqa
-            'tags': [
-                'Strasse',
-                'Strassenverkehr',
-                'Verkehrsnetz',
-                'Strassennetz',
-                'Nationalstrassen',
-                'Staatsstrassen',
-                'Kanton Zürich'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1118',
+            'tags': ['Strasse', 'Strassenverkehr', 'Verkehrsnetz', 'Strassennetz', 'Nationalstrassen', 'Staatsstrassen', 'Kanton Zürich']
         },
         '86e20e7c-29fe-4840-8229-a540211d7bb5': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1117',  # noqa
-            'tags': [
-                'Strasse',
-                'Strassenverkehr',
-                'Verkehrsnetz',
-                'Strassennetz',
-                'Nationalstrassen',
-                'Staatsstrassen',
-                'Kanton Zürich'
-            ]
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1117',
+            'tags': ['Strasse', 'Strassenverkehr', 'Verkehrsnetz', 'Strassennetz', 'Nationalstrassen', 'Staatsstrassen', 'Kanton Zürich']
+        },
+        'eda09949-0750-4eac-ba2b-6c78ca54d69e': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1097',
+            'tags': []
         },
         'a35fcf31-56f4-4b6d-b90a-54770a71bd8d': {
-            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1097',  # noqa
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1178',
             'tags': []
+        },
+        '9f1cfeee-c40e-4e5b-84ba-f6c19048bcad': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1217',
+            'tags': ['Tiefbau', 'Strassen', 'Verkehr', 'Baustellen', 'Verkehrsbehinderung']
+        },
+        'e112d147-3b8e-42de-9b94-0ae23b05e3be': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1297',
+            'tags': []
+        },
+        'fc314cc0-6916-42d6-96ca-18dd9394d02d': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1277',
+            'tags': ['Wasser', 'Fluss', 'Bach', 'Ökomorphologie', 'Gewässer', 'Verbauung', 'Uferbereich']
+        },
+        '22b0d59f-363c-4643-929b-8a06cddf7f75': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1197',
+            'tags': ['Freianlagen', 'Sporthallen', 'Bäder', 'Eissportanlagen']
+        },
+        '2999470f-b6c8-458f-b10d-a55d2d86abde': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1238',
+            'tags': ['Velo', 'Radweg', 'Veloinfrastruktur', 'Velomarkierung', 'Radstreifen', 'Busspur']
+        },
+        '46cd5817-5ac4-4de9-909d-233bd319a6b9': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1237',
+            'tags': ['Velo', 'Parkierung', 'öffentlich', 'Veloparkieranlage', 'Anlagen']
+        },
+        'b7ce6cfb-8ce0-41dc-8e24-0a0dff2d9aba': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1260',
+            'tags': ['Verkehr', 'Verkehrsmessstellen', 'Verkehrszählstellen', 'Verkehrsmodell']
+        },
+        '9a4cc889-7907-454b-9d99-5b12c450e3ca': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1177',
+            'tags': []
+        },
+        '7693752a-6294-4087-ae5e-83e8a0176315': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1258',
+            'tags': ['Tiefbau', 'Strassen', 'Verkehr', 'Baustellen', 'Verkehrsbehinderung']
+        },
+        '8174aafe-3b3b-4e44-9a70-96bb6064018a': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1298',
+            'tags': []
+        },
+        '6a4b6960-2c21-420c-b36d-d7dd3c184643': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1262',
+            'tags': []
+        },
+        '2024fa34-6418-4de1-85f5-c619bda2c961': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1261',
+            'tags': ['Velo', 'Parkierung', 'öffentlich', 'Veloparkieranlage', 'Anlagen']
+        },
+        '0db7eda5-2a95-4bbf-9b10-d1942bd85ca7': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1259',
+            'tags': ['Verkehr', 'Verkehrsmessstellen', 'Verkehrszählstellen', 'Verkehrsmodell']
+        },
+        '4e27e579-88cf-4fb1-be90-869a9caeeac3': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1257',
+            'tags': ['Wald']
+        },
+        '3479db7a-30e7-4505-b452-977852a187c9': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1320',
+            'tags': ['Schutzwald', 'gravitative Naturgefahren', 'Schadenpotential', 'Schutzfunktion']
+        },
+        '5117c450-b502-42c1-823b-128bce2b1997': {
+            'geolion_url': 'http://www.geolion.zh.ch/geodatenservice/show?nbid=1319',
+            'tags': ['WNB']
         },
     }
 
